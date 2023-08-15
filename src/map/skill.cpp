e13624fe69245a93aa4cc94cc2da1e0a76c78eb6
eppc0330 <47050704+eppc0330@users.noreply.github.com>
2023-08-15 15:13:30 +0900

    Change clif damage of jumpattack

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -3957,7 +3957,10 @@ int64 skill_attack (int attack_type, struct block_list* src, struct block_list *
 					break;
 				}
 			}
-			dmg.dmotion = clif_skill_damage(dsrc,bl,tick, dmg.amotion, dmg.dmotion, damage, dmg.div_, skill_id, flag&SD_LEVEL?-1:skill_lv, dmg_type);
+			if (sd && sd->state.jumpattack == 1 && skill_id == 0) {
+				dmg.dmotion = clif_damage(src,bl,tick,dmg.amotion,dmg.dmotion,dmg.damage,dmg.div_,dmg.type,dmg.damage2,false);
+			} else
+				dmg.dmotion = clif_skill_damage(dsrc,bl,tick, dmg.amotion, dmg.dmotion, damage, dmg.div_, skill_id, flag&SD_LEVEL?-1:skill_lv, dmg_type);
 			break;
 	}
 
